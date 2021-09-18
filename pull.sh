5c1e5a9907dc60beef2978e174ecce08ce41c1e2
wanglu <wanglu@mereith.com>
2021-09-18 18:02:18 +0800

    fix: script

--- pull.sh
+++ pull.sh
@@ -10,7 +10,7 @@ else
     git config --global credential.helper store && \
     git config --global user.name ${GIT_USERNAME} && \
     git config --global user.email ${GIT_EMAIL} && \
-    cd usr/local/authing-dev && \
+    cd /usr/local/authing-dev && \
     git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@git.authing.co/authing-next/authing-server.git && \
     git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@git.authing.co/authing-next/authing-fe-console.git && \
     git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@git.authing.co/authing-next/authing-user-portal.git
