4fba1029f77651199bc69d5d12658d5e94e29063
wanglu <wanglu@mereith.com>
2021-09-23 14:50:51 +0800

    修改 pull.sh

--- pull.sh
+++ pull.sh
@@ -6,12 +6,12 @@ echo "pull.sh <USER_NAME> <PASSWORD>"
 elif [ "$2" == "" ]; then
 echo "pull.sh <USER_NAME> <PASSWORD>"
 else
-    echo 'https://${GIT_USERNAME}:${GIT_PASSWORD}@git.authing.co' > /root/.git-credentials && \
-    git config --global credential.helper store && \
-    git config --global user.name ${GIT_USERNAME} && \
-    git config --global user.email ${GIT_EMAIL} && \
-    cd /usr/local/authing-dev && \
-    git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@git.authing.co/authing-next/authing-server.git && \
-    git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@git.authing.co/authing-next/authing-fe-console.git && \
+    echo 'https://${GIT_USERNAME}:${GIT_PASSWORD}@git.authing.co' > /root/.git-credentials
+    git config --global credential.helper store
+    git config --global user.name ${GIT_USERNAME}
+    git config --global user.email ${GIT_EMAIL}
+    cd /usr/local/authing-dev
+    git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@git.authing.co/authing-next/authing-server.git
+    git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@git.authing.co/authing-next/authing-fe-console.git
     git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@git.authing.co/authing-next/authing-user-portal.git
 fi
