a54270dcb3058bdbb70aa5e35ff12229170fa1ad
wanglu <wanglu@mereith.com>
2021-09-23 11:30:21 +0800

    基本完成

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+npm -g install yarn
 yarn global add typescript
 cd authing-server && yarn && cd src/packages/samlify && tsc && rm -rf ../../../node_modules/samlify/* && mv ./build/* ../../../node_modules/samlify/  && cd ../../../../ && \
 cd authing-fe-console && yarn && cd .. && \
