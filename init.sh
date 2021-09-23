4cd6fd57e51184cd0358299bb376e70028073614
wanglu <wanglu@mereith.com>
2021-09-23 16:35:57 +0800

    fix: tyarn

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 npm -g install yarn
+yarn global add tyarn
 yarn global add typescript
-cd authing-server && yarn && cd src/packages/samlify && tsc && rm -rf ../../../node_modules/samlify/* && mv ./build/* ../../../node_modules/samlify/  && cd ../../../../ && \
-cd authing-fe-console && yarn && cd .. && \
-cd authing-user-portal && yarn && cd ..
+cd authing-server && tyarn && cd src/packages/samlify && tsc && rm -rf ../../../node_modules/samlify/* && mv ./build/* ../../../node_modules/samlify/  && cd ../../../../ && \
+cd authing-fe-console && tyarn && cd .. && \
+cd authing-user-portal && tyarn && cd ..
